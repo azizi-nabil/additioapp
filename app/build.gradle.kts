e1a20c349ef8786007011683bc5eae1f2e1ffe2d
Nabil Azizi <azizi.nabil@proton.me>
2026-01-13 19:18:40 +0100

    Bump version to 3.6.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.teacherhub"
         minSdk = 24
         targetSdk = 34
-        versionCode = 16
-        versionName = "3.5.2"
+        versionCode = 17
+        versionName = "3.6.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
